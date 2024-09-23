fdaf0a2d38b0e56138f9077525afff9abd380438
wohu8292 <wohu8292@gmail.com>
2024-09-22 21:44:10 -0600

    d

---
+++ Projects/InteractiveWeb/src/components/ModelView.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from '@react-three/drei'
+
+const ModelView= ({index, groupRef, gsapType, controlRef, setRotationSize, size, item})=>{
+        return (
+            <View
+            index={index}
+            id='gsapType'
+            className={'border-2 border-red-500 w-full h-full'}>
+
+            </View>
+        )
+    }
+
+export default ModelView
